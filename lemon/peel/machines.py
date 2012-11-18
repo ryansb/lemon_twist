45598cd1e0010262a282b4500bb2f563a41d3853
Ryan Brown <ryansb@csh.rit.edu>
2012-11-18 14:36:33 -0800

    Add __repr__ for Machine and Slot objects, add Slot primary key

--- lemon/peel/machines.py
+++ lemon/peel/machines.py
@@ -9,12 +9,14 @@ from sqlalchemy.orm import relationship
 
 
 class Machine(Base):
-    __tablename__ = 'machines',
+    __tablename__ = 'machines'
     mid = sa.Column(sa.SmallInteger(unsigned=True),
                     primary_key=True,
                     nullable=False)
     machine_name = sa.Column(sa.String(length=255),
                              nullable=True)
+    short_name = sa.Column(sa.String(length=7),
+                           nullable=True)
     #slots a list of all the slots in the machine
     deleted = sa.Column(sa.Boolean,
                         default=False)
@@ -36,16 +38,32 @@ class Machine(Base):
     location = sa.Column(sa.String(length=255),
                          nullable=True)
 
+    def __repr__(self):
+        if self.deleted:
+            return ("<Deleted Machine %s (%s), mid: %s public_ip: %s "
+                    "connections allowed: %s>" % (
+                        self.machine_name, self.short_name, self.mid,
+                        self.public_ip, self.allow_connect)
+                    )
+        return ("<Machine %s (%s), mid: %s public_ip: %s "
+                "connections allowed: %s>" % (
+                    self.machine_name, self.short_name, self.mid,
+                    self.public_ip, self.allow_connect)
+                )
+
 
 class Slot(Base):
-    __tablename__ = 'slots',
+    __tablename__ = 'slots'
+    id = sa.Column(sa.Integer,
+                   primary_key=True,
+                   nullable=False)
     mid = sa.Column(sa.SmallInteger(unsigned=True),
                     sa.ForeignKey('machines.mid'),
                     nullable=False)
     machine = relationship('Machine',
                            backref='slots',
-                           primaryjoin="Machine.mid==Slot.mid ORDER BY Slot.slot_num")
-    slot_num = sa.Column(sa.TinyInteger(unsigned=True),
+                           order_by="Slot.slot_num")
+    slot_num = sa.Column(sa.SmallInteger(unsigned=True),
                          nullable=False)
     slot_name = sa.Column(sa.String(length=255),
                           nullable=True)
@@ -54,3 +72,8 @@ class Slot(Base):
     availability = sa.Column(sa.SmallInteger(unsigned=True),
                              nullable=False,
                              default=0)
+
+    def __repr__(self):
+        return "<Slot #%s in %s, name: \"%s\" price: %s available: %s>" % (
+            self.slot_num, self.machine.machine_name, self.slot_name,
+            self.price, self.availability)
